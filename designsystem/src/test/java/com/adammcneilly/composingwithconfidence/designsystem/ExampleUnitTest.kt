8bac91e22c81c92d56ab7daddea164f1e941ea65
adammc331 <amcneilly331@gmail.com>
2022-08-28 17:14:19 -0400

    Suppressing magic numbers.

--- designsystem/src/test/java/com/adammcneilly/composingwithconfidence/designsystem/ExampleUnitTest.kt
+++ designsystem/src/test/java/com/adammcneilly/composingwithconfidence/designsystem/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.adammcneilly.composingwithconfidence.designsystem
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
