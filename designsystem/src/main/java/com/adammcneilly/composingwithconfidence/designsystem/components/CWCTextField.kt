6cae87fecaafd8d024d01adb890e2a3872c13523
adammc331 <amcneilly331@gmail.com>
2022-08-29 21:18:38 -0400

    Adding text foield and paparazzi utils.

---
+++ designsystem/src/main/java/com/adammcneilly/composingwithconfidence/designsystem/components/CWCTextField.kt
@@ -0,0 +1,88 @@
+package com.adammcneilly.composingwithconfidence.designsystem.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults.TextFieldShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+/**
+ * This is a custom implementation of an [OutlinedTextField] to ensure that it has the TOA branding
+ * and styling that we expect.
+ *
+ * @param[text] The current text inside the input.
+ * @param[onTextChanged] A callback invoked whenever the user modifies the text inside this input.
+ * @param[labelText] The label that shows above the input when focused.
+ * @param[modifier] An optional [Modifier] to configure this component.
+ * @param[focusRequester] An optional [FocusRequester] that allows the caller to control the focused
+ * state of this text field.
+ */
+@Composable
+fun CWCTextField(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    labelText: String?,
+    modifier: Modifier = Modifier,
+    errorMessage: String? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    enabled: Boolean = true,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    placeholderText: String? = null,
+    focusRequester: FocusRequester = FocusRequester(),
+) {
+    val labelComposable: (@Composable () -> Unit)? = labelText?.let {
+        @Composable {
+            Text(
+                text = labelText,
+            )
+        }
+    }
+
+    val placeholderComposable: (@Composable () -> Unit)? = placeholderText?.let {
+        @Composable {
+            Text(
+                text = placeholderText,
+            )
+        }
+    }
+
+    Column {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChanged,
+            label = labelComposable,
+            shape = TextFieldShape,
+            modifier = modifier
+                .heightIn(48.dp)
+                .fillMaxWidth()
+                .focusRequester(focusRequester),
+            isError = (errorMessage != null),
+            visualTransformation = visualTransformation,
+            enabled = enabled,
+            keyboardOptions = keyboardOptions,
+            placeholder = placeholderComposable,
+        )
+
+        if (errorMessage != null) {
+            Text(
+                text = errorMessage,
+                color = MaterialTheme.colors.error,
+                modifier = Modifier
+                    .padding(
+                        top = 4.dp,
+                        start = 16.dp,
+                    ),
+            )
+        }
+    }
+}
--- designsystem/src/test/java/com/adammcneilly/composingwithconfidence/designsystem/components/PrimaryButtonPaparazziTest.kt
+++ designsystem/src/test/java/com/adammcneilly/composingwithconfidence/designsystem/components/PrimaryButtonPaparazziTest.kt
@@ -1,10 +1,5 @@
 package com.adammcneilly.composingwithconfidence.designsystem.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import app.cash.paparazzi.Paparazzi
 import org.junit.Rule
 import org.junit.Test
@@ -15,35 +10,23 @@ class PrimaryButtonPaparazziTest {
 
     @Test
     fun renderEnabledButton() {
-        paparazzi.snapshot {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-            ) {
-                PrimaryButton(
-                    text = "Enabled Button",
-                    onClick = {},
-                    enabled = true,
-                )
-            }
+        paparazzi.snapshotScreen {
+            PrimaryButton(
+                text = "Enabled Button",
+                onClick = {},
+                enabled = true,
+            )
         }
     }
 
     @Test
     fun renderDisabledButton() {
-        paparazzi.snapshot {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-            ) {
-                PrimaryButton(
-                    text = "Disabled Button",
-                    onClick = {},
-                    enabled = false,
-                )
-            }
+        paparazzi.snapshotScreen {
+            PrimaryButton(
+                text = "Disabled Button",
+                onClick = {},
+                enabled = false,
+            )
         }
     }
 }
