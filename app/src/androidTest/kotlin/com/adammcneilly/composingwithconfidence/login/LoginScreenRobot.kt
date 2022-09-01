689ad00d0910ce0f6db5e6ea77ac194a71b2e8c0
Adam McNeilly <amcneilly331@gmail.com>
2022-08-31 23:33:45 -0400

    Adding test documentation. (#5)

--- app/src/androidTest/kotlin/com/adammcneilly/composingwithconfidence/login/LoginScreenRobot.kt
+++ app/src/androidTest/kotlin/com/adammcneilly/composingwithconfidence/login/LoginScreenRobot.kt
@@ -7,7 +7,15 @@ import androidx.compose.ui.test.junit4.ComposeTestRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
+import com.adammcneilly.composingwithconfidence.home.HomeScreenRobot
 
+/**
+ * This is a test robot that defines all of the components on the login screen that we may want
+ * to interact with, and exposes public functions allowing us to do so.
+ *
+ * This helps with re-usability, so any tests that touch the login screen can reference this code, and if our
+ * login screen changes this is all we have to update.
+ */
 class LoginScreenRobot(
     composeTestRule: ComposeTestRule,
 ) {
@@ -41,6 +49,10 @@ class LoginScreenRobot(
     }
 }
 
+/**
+ * This works some Kotlin magic so we can call a function to create a [LoginScreenRobot] and
+ * pass in a function of all the actions to be triggered on this screen.
+ */
 fun loginScreenRobot(
     composeTestRule: ComposeTestRule,
     block: LoginScreenRobot.() -> Unit,
